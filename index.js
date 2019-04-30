b8b0e30acbad347d8e771105d5c8a778b5f3e3ab
Darien Payton <darien.payton71@gmail.com>
2019-04-30 12:55:53 -0400

    added post operation

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ server.use(helmet());
 
 // endpoints here
 
+server.post('/api/zoos', (req, res) => {
+  const zooName = req.body
+  if(zooName) {
+    db.insert(zooName)
+    .into('zoos')
+    .then(ids => {
+      res.status(201).json(ids[0])
+    })
+    .catch(() => {
+      res.status(500).send('System had a problem adding the new zoo')
+    })
+  } else {
+    return res.status(404).json({error: 'Please insert a zoo name'})
+  }
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
